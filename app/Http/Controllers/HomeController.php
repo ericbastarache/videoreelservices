f02e676e975bb5ba558fff0cb8d256224913e0d7
ericb <eric_bastarache@hotmail.com>
2017-03-14 00:02:44 -0400

    Added dashboard function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+        return view('index');
+    }
+
+    public function dashboard ()
+    {
         return view('home');
     }
 }
