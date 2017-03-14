24cdf4bb345e5cd54b2d2e7018d3dd385e5a2027
ericb <eric_bastarache@hotmail.com>
2017-03-14 01:46:38 -0400

    Updated preliminary footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,14 @@
 
 @section('content')
 
-
+<footer class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6"></div>
+      <div class="col-md-6"></div>
+    </div>
+  </div>
+</footer>
 
 
 @endsection
